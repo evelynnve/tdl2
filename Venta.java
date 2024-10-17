1d6956832603b106e3a2e04830183a376f0981b3
rodricomp <rodrigomendizabal123@hotmail.com>
2024-10-17 19:34:45 -0300

    Update Venta.java

--- Venta.java
+++ Venta.java
@@ -1,1 +1,17 @@
+package Entregable;
 
+import java.util.List;
+
+/** esta clase representa la compra y venta de criptomonedas
+ * @author Grupo34
+ * @version 1.0
+ *  
+ */
+
+public class Venta extends Transacciones {
+
+	
+	public Venta () {
+		
+	}
+}
