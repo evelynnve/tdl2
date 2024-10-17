970183fe1251d08f4868dbcde4c0d73ff91b47af
Gonzalo Montangie <118616955+GonzaMontangie@users.noreply.github.com>
2024-10-17 19:45:30 -0300

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,17 @@
+package Entregable;
+
+import java.util.List;
+
+/** esta clase representa el swap de criptomonedas
+ * @author Grupo34
+ * @version 1.0
+ *  
+ */
+
+public class Swap extends Transacciones {
+
+	
+	public Swap () {
+		
+	}
+}
