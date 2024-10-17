9c780e1d2fa8290325a6c5362e6de2c24105364e
evelynnve <146297781+evelynnve@users.noreply.github.com>
2024-10-17 20:03:04 -0300

    Update Enviar.java

--- Enviar.java
+++ Enviar.java
@@ -1,6 +1,14 @@
-package Entregable;
+package Entrgable;
 
-public class EnviarRecibir {
+import java.util.List;
+
+/** esta clase representa el envio de criptomonedas
+ * @author Grupo34
+ * @version 1.0
+ *  
+ */
+
+public class Enviar {
 	
 	public Enviar() {
 		
