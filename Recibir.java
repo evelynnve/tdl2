73d127548c299386696f5f8918bd960d8908a8be
Gonzalo Montangie <118616955+GonzaMontangie@users.noreply.github.com>
2024-10-17 19:41:05 -0300

    Create Recibir.java

---
+++ Recibir.java
@@ -0,0 +1,17 @@
+package Entrgable;
+
+import java.util.List;
+
+/** esta clase representa la recibir de criptomonedas
+ * @author Grupo34
+ * @version 1.0
+ *  
+ */
+
+public class Recibir extends Transacciones {
+
+	
+	public Recibir () {
+		
+	}
+}
