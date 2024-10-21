e7d2f6ef283f4205425492fd760a995e997f67e8
rodricomp <rodrigomendizabal123@hotmail.com>
2024-10-21 01:07:28 -0300

    Create CompararNomenclatura.java

---
+++ CompararNomenclatura.java
@@ -0,0 +1,12 @@
+package Conexion;
+
+import java.util.Comparator;
+
+public class CompararNomenclatura implements Comparator<Moneda> {
+	public int compare(Moneda m1, Moneda m2) {
+		if (!(m1.getSimbolo().equals(m2.getSimbolo())))
+			return m1.getSimbolo().compareTo(m2.getSimbolo());
+		else
+			return m1.getNombre().compareTo(m2.getNombre());
+	}
+}
